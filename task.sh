bdb019a11b1dce42711089f3f9f6ad7eeaadc2a0
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-11-30 17:13:36 +0200

    optimize portability

--- task.sh
+++ task.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir /home/arwa/myDirectory
-mkdir /home/arwa/myDirectory/secondDirectory
+mkdir -p /home/arwa/myDirectory/secondDirectory
 touch /home/arwa/myDirectory/secondDirectory/myNotePaper
 cp /home/arwa/myDirectory/secondDirectory/myNotePaper  /home/arwa/myDirectory/
 mv /home/arwa/myDirectory/myNotePaper /home/arwa/myDirectory/myOldNotePaper
-
