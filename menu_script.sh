589b782e61ef49016763dce9087f8596d8bd8165
arwaghaith <arwagheith80@gmail.com>
2023-12-04 17:31:11 +0200

    uploading the exam

---
+++ menu_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "This is a menu program"
+showdate()
+{
+
+echo `date` 
+}
+
+showSysUptime()
+{
+
+echo `uptime`
+}
+showCurrentUsers()
+{
+echo `who` 
+}
+choice=1
+while [ $choice != 0 ]
+do
+    echo "************************************************" 
+    echo "Enter 1 to display the current date and time"
+    echo "Enter 2 to display the system uptime"
+    echo "Enter 3 to display information about logged-in users"
+    echo "Enter 0 to exit the menu"
+    echo  "Enter your choice"
+    echo  "Your choice is: "
+    read -r choice
+    case $choice in
+        1)
+            showdate 
+            ;;
+        2)
+            showSysUptime 
+            ;;
+        3)
+            
+           showCurrentUsers 
+            ;;
+        0)
+            echo "You are exiting the menu"
+            ;;
+        *)
+            echo "Please enter a valid choice"
+            ;;
+    esac
+done
